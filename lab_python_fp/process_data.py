027750d640f1a270f7a73646e56cb18707ee5610
rowa <rowa@rowas-MacBook-Pro.local>
2025-11-13 14:40:29 +0300

    fixed process_data

--- lab_python_fp/process_data.py
+++ lab_python_fp/process_data.py
@@ -1,18 +1,19 @@
 import json
-import sys
+import os
 from print_result  import print_result
 from cm_timer import cm_timer_1
 from gen_random import gen_random
 from unique import Unique
 
-path = "lab_python_fp/data_light.json"
+path = os.path.join(os.path.dirname(__file__), "data_light.json")
 
 with open(path) as f:
     data = json.load(f)
 
 @print_result
 def f1(vacancies):
-    return sorted(Unique(vacancies, case_sensitive=False))
+    jobs = [vacancy["job-name"] for vacancy in vacancies]
+    return sorted(Unique(jobs, case_sensitive=False))
 
 @print_result
 def f2(jobs):
@@ -25,8 +26,10 @@ def f3(jobs):
 @print_result
 def f4(jobs):
     salaries = gen_random(len(jobs), 100_000, 200_000)
+    result = []
     for job, salary in zip(jobs, salaries):
-        return [f"{job}, зарплата {salary} руб."]
+        result.append(f"{job}, зарплата {salary} руб.")
+    return result
 
 if __name__ == '__main__':
     with cm_timer_1():
